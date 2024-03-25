79f66322c227c239ad8c5eb9bb73dd21ff51cd19
Neil MacLean <nbmac13@gmail.com>
2024-03-25 16:29:19 -0500

    last bit of set up

---
+++ src/components/hero/Hero.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Hero.css';
+
+export const Hero: React.FC = () => {
+    return (
+        <div className="hero">
+            <h1>Welcome to My Portfolio</h1>
+            <p>Discover my amazing projects and skills</p>
+        </div>
+    );
+};
---
+++ src/components/navbar/NavBar.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from '../../nm_logo.svg';
+
+export const NavBar: React.FC = () => {
+    return (
+        <nav id="navBar">
+            <ul>
+                <li>
+                    <a href="#hero"><img src={logo} className="App-logo" alt="logo" /></a>
+                </li>
+                <li>
+                    <a href="#about">About</a>
+                </li>
+                <li>
+                    <a href="#stack">Stack</a>
+                </li>
+                <li>
+                    <a href="#projects">Projects</a>
+                </li>
+                <li>
+                    <a href="#contact">Contact</a>
+                </li>
+            </ul>
+        </nav>
+    );
+};
